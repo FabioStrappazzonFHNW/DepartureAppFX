85bc36654e705c423e3269f089766a6e313a65f4
Fabio Strappazzon <fabio.strappazzon@students.fhnw.ch>
2016-04-26 14:56:53 +0200

    initial commit

---
+++ src/ch/fhnw/oop2/departure/controller/RootLayoutController.java
@@ -0,0 +1,14 @@
+package ch.fhnw.oop2.departure.controller;
+
+import ch.fhnw.oop2.departure.MainApp;
+
+/**
+ * Created by Matthias Ernst on 26.04.2016.
+ */
+public class RootLayoutController {
+    private MainApp mainApp;
+
+    public void setMainApp(MainApp mainApp) {
+        this.mainApp = mainApp;
+    }
+}
---
+++ src/ch/fhnw/oop2/departure/model/Departure.java
@@ -0,0 +1,16 @@
+package ch.fhnw.oop2.departure.model;
+
+import javafx.beans.property.SimpleStringProperty;
+
+public class Departure {
+	final private SimpleStringProperty id;
+	final private SimpleStringProperty departureTime;
+	final private SimpleStringProperty trainNumber;
+	final private SimpleStringProperty direction;
+	final private SimpleStringProperty via;
+	final private SimpleStringProperty platform;
+	
+	public Departure() {
+
+	}
+}
